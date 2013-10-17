20f3b0d70e1a1e69aa920c56e3b7377591ca4f87
willyhusted <willyhusted@comcast.net>
2013-10-17 10:42:08 -0700

    Delete powers.go

--- powers.go
+++
@@ -1,7 +0,0 @@
-func powers(base int, limit int, callback func(int)) {
-    result := 1
-        for result <= limit {
-            callback(result)
-            result = result * base
-            }
-}
